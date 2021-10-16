00d6cbd7f97431d153aceb2574539dc071df929a
Graham Beaumont <grb011@student.usc.edu.au>
2021-10-16 16:32:17 +1000

    Started to implement the Database

--- app/src/main/java/com/bignerdranch/android/plainolnotes/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/plainolnotes/MainFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bignerdranch.android.plainolnotes.databinding.MainFragmentBinding
@@ -23,6 +25,10 @@ class MainFragment : Fragment(),
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        (activity as AppCompatActivity)
+            .supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
         binding = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
@@ -45,6 +51,8 @@ class MainFragment : Fragment(),
     }
 
     override fun onItemClick(noteId: Int) {
-        Log.i(TAG, "onItemClick: recieved note id $noteId")
+        Log.i(TAG, "onItemClick: received note id $noteId")
+        val action = MainFragmentDirections.actionEditNote(noteId)
+        findNavController().navigate(action)
     }
 }
