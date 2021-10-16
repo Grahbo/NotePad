5a6d449b903a94f7fe20e07ca3bb4bd5298bc0fc
Graham Beaumont <grb011@student.usc.edu.au>
2021-10-16 16:36:40 +1000

    Implemented Date Long convertor for database

---
+++ app/src/main/java/com/bignerdranch/android/plainolnotes/data/DateConvertor.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.plainolnotes.data
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class DateConvertor {
+
+    @TypeConverter
+    fun fromTimeStamp(value: Long): Date{
+        return Date(value)
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date): Long {
+        return date.time
+    }
+}
